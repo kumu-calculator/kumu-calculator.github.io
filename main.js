202c57e9574d2a543d0786efaa3abf04e7f08ba0
kumu-calculator <103378049+kumu-calculator@users.noreply.github.com>
2022-04-11 01:05:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 document.title = "Kumu Calculator";
-document.body.innerHTML = [
-  "<h1>Kumu Calculator</h1>",
-  "<p>A simple calculator to estimate total diamonds earned for campaigns when converting co-host earned diamonds to coins repeatedly</p>"
-].join("");
+
+window.addEventListener('load', function () {
+  document.body.innerHTML = [
+    "<h1>Kumu Calculator</h1>",
+    "<p>A simple calculator to estimate total diamonds earned for campaigns when converting co-host earned diamonds to coins repeatedly</p>"
+  ].join("");
+});
